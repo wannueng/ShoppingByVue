cda79c3c2fd675988fd9f65f6435d109cf1924a5
wannueng <1411597241@qq.com>
2018-07-25 21:39:29 +0800

    电商项目的初始化（vue）

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由模块
+import VueRouter from 'vue-router'
+// 引入index组件
+import index from  './components/index'
+// 使用路由中间件
+Vue.use(VueRouter)
+const router=new VueRouter({
+  routes:[{
+    path:'/index',
+    component:index
+  }]
+})
 
+
+// 引入css
+import './assets/statics/site/css/style.css';
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
