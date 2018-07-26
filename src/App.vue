518540816dd113c74666d9a23bf68532b25c518f
wannueng <1411597241@qq.com>
2018-07-26 20:32:21 +0800

    完成首页的渲染

--- src/App.vue
+++ src/App.vue
@@ -119,12 +119,29 @@
 </template>
 
 <script>
+import $ from 'jquery'
  export default{
+    
+mounted() {
+    	$("#menu2 li a").wrapInner( '<span class="out"></span>' );
+	$("#menu2 li a").each(function() {
+		$( '<span class="over">' +  $(this).text() + '</span>' ).appendTo( this );
+	});
 
+	$("#menu2 li a").hover(function() {
+		$(".out",	this).stop().animate({'top':	'48px'},	300); // move down - hide
+		$(".over",	this).stop().animate({'top':	'0px'},		300); // move down - show
+
+	}, function() {
+		$(".out",	this).stop().animate({'top':	'0px'},		300); // move up - show
+		$(".over",	this).stop().animate({'top':	'-48px'},	300); // move up - hide
+	});
+},
  }
 </script>
 
 <style>
  /* 标准的 css中导入其他css的语法 */
-  /* @import url('./assets/statics/site/css/style.css'); */
+  @import url('./assets/lib/hoverNav20151228/css/style.css');
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,29 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 // 引入index组件
 import index from  './components/index'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueLazyload from 'vue-lazyload'
+import path from 'path'
 // 使用路由中间件
 Vue.use(VueRouter)
+// 使用element-ui
+Vue.use(ElementUI);
+Vue.use(VueLazyload,{
+  loading: path.join(__dirname,'/assets/01.gif'),
+});
 const router=new VueRouter({
-  routes:[{
+  routes:[
+    {
+      path:'/',
+      redirect:'index'
+    },
+    {
     path:'/index',
     component:index
-  }]
+  }
+]
 })
 
 
